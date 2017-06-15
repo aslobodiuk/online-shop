c0761c63ae7da88c712cd91286bd1cac4dbccb0e
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-06-15 14:41:21 +0300

    change cart class

--- cart/cart.py
+++ cart/cart.py
@@ -7,16 +7,12 @@ from shop.models import Product
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
-        if not cart:
-            cart = self.session[settings.CART_SESSION_ID] = {}
+        cart = self.session.get(settings.CART_SESSION_ID, {})
         self.cart = cart
 
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
+        self.cart.setdefault(product_id, {'quantity': 0, 'price': str(product.price)})
         if update_quantity:
             if product.stock < quantity:
                 return True
@@ -40,15 +36,15 @@ class Cart(object):
             self.save()
 
     def __iter__(self):
-            product_ids = self.cart.keys()
-            products = Product.objects.filter(id__in=product_ids)
-            for product in products:
-                self.cart[str(product.id)]['product'] = product
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
 
-            for item in self.cart.values():
-                item['price'] = Decimal(item['price'])
-                item['total_price'] = item['price'] * item['quantity']
-                yield item
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
